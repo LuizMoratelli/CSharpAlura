2df71cbcf60bc6c9ee359afb156a213d05e7f423
Luiz Augusto Moratelli <gameplayteam12@gmail.com>
2018-12-10 23:43:51 -0200

    Curso 6
    Tratamento de String

---
+++ ByteBankDll/ByteBankDll.SistemaAgencia/ExtratatorValorDeArgumentosURL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankDll.SistemaAgencia
+{
+    public class ExtratatorValorDeArgumentosURL
+    {
+        public string URL { get; }
+        private readonly string _argumentos;
+
+        // ctor = criar construtor
+        public ExtratatorValorDeArgumentosURL(string url) {
+            if (String.IsNullOrEmpty(url)) {
+                throw new ArgumentException("O argumento URL não pode ser nulo nem vazio.", nameof(url));
+            }
+            //if (url == null)
+            //    throw new ArgumentNullException(nameof(url));
+            //else if (url == "")
+            //    throw new ArgumentException("O argumento URL não pode ser uma string vazia.", nameof(url));
+
+            URL = url;
+
+            // Prefere-se utilizar o paramtro à propriedade, pois caso a propriedade seja movida no codigo o mesmo não terá efeito
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao++);
+        }
+
+        public string GetValor(string nomeParametro) {
+            string termo = nomeParametro + "=";
+            int indiceTermo = _argumentos.IndexOf(termo);
+
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length);
+            int indiceEComercial = resultado.IndexOf('&');
+
+            if (indiceEComercial == -1)
+                return resultado;
+
+            return resultado.Remove(indiceEComercial);
+        }
+    }
+}
--- ByteBankDll/ByteBankDll.SistemaAgencia/Program.cs
+++ ByteBankDll/ByteBankDll.SistemaAgencia/Program.cs
@@ -13,18 +13,30 @@ namespace ByteBankDll.SistemaAgencia
     class Program
     {
         static void Main(string[] args) {
-            ContaCorrente conta = new ContaCorrente(123, 133);
+            string url = "pagina?argumentos";
+
+            int indiceInterrogacao = url.IndexOf('?');
+            // Classe string é imutavel
+            // Substring é inclusivo, inclue o indice solicitado
+            string argumentos = url.Substring(indiceInterrogacao++);
+
+            ExtratatorValorDeArgumentosURL extrator = new ExtratatorValorDeArgumentosURL("pagina?url=123&numero=123");
+            Console.WriteLine(extrator.GetValor("url"));
+
+            //Console.WriteLine(argumentos);
+
+            //ContaCorrente conta = new ContaCorrente(123, 133);
             
-            DateTime dataFimPagamento = new DateTime(2018, 8, 17);
-            DateTime dataCorrente = DateTime.Now;
+            //DateTime dataFimPagamento = new DateTime(2018, 8, 17);
+            //DateTime dataCorrente = DateTime.Now;
 
-            // Quando subtrai-se uma data de outra utiliza-se TimeSpan
-            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+            //// Quando subtrai-se uma data de outra utiliza-se TimeSpan
+            //TimeSpan diferenca = dataFimPagamento - dataCorrente;
 
-            // Ferramentas >> Gerenciador de Pacotes Nuget >> Console
-            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            //// Ferramentas >> Gerenciador de Pacotes Nuget >> Console
+            //string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
 
-            Console.WriteLine(mensagem);
+            //Console.WriteLine(mensagem);
             Console.ReadLine();
         }
     }

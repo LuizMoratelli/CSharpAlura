4f0a7a5c0936fefa3b763c301ed717fbe1aca1f5
Luiz Augusto Moratelli <gameplayteam12@gmail.com>
2019-02-13 23:32:50 -0200

    Início do Projeto do Xadrez

---
+++ xadrez-console/xadrez-console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine(p);
+        }
+    }
+}

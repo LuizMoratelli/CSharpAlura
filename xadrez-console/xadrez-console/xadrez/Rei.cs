94240c3dd8525f958106a5bbd5b67656cfd33304
Luiz Augusto Moratelli <gameplayteam12@gmail.com>
2019-02-18 23:58:54 -0300

    Jogadas Especiais Roque Grande e Pequeno

--- xadrez-console/xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez-console/xadrez/Rei.cs
@@ -5,14 +5,22 @@ namespace xadrez
 {
     class Rei : Peca
     {
-        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
-        {
+        private PartidaDeXadrez partida;
 
+        public Rei(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
+        {
+            this.partida = partida;
         }
 
         public override string ToString()
         {
             return "R";
+        }
+
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = Tab.GetPeca(pos);
+            return p != null && p is Torre && p.CorAtual == CorAtual && p.QuantidadeMovimentos == 0;
         }
 
         public override bool[,] MovimentosPossiveis()
@@ -74,6 +82,38 @@ namespace xadrez
             if (Tab.PosicaoValida(pos) && PodeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //#jogadaespecial roque
+            if (QuantidadeMovimentos == 0 && !partida.xeque)
+            {
+                Console.WriteLine("Entrou");
+                //Roque pequeno
+                Posicao posicaoTorre1 = new Posicao(PosicaoAtual.Linha, PosicaoAtual.Coluna + 3);
+                if (testeTorreParaRoque(posicaoTorre1))
+                {
+                    Posicao p1 = new Posicao(PosicaoAtual.Linha, PosicaoAtual.Coluna + 1);
+                    Posicao p2 = new Posicao(PosicaoAtual.Linha, PosicaoAtual.Coluna + 2);
+
+                    if (Tab.GetPeca(p1) == null && Tab.GetPeca(p2) == null)
+                    {
+                        mat[PosicaoAtual.Linha, PosicaoAtual.Coluna + 2] = true;
+                    }
+                }
+
+                //Roque grande
+                Posicao posicaoTorre2 = new Posicao(PosicaoAtual.Linha, PosicaoAtual.Coluna - 4);
+                if (testeTorreParaRoque(posicaoTorre2))
+                {
+                    Posicao p1 = new Posicao(PosicaoAtual.Linha, PosicaoAtual.Coluna - 1);
+                    Posicao p2 = new Posicao(PosicaoAtual.Linha, PosicaoAtual.Coluna - 2);
+                    Posicao p3 = new Posicao(PosicaoAtual.Linha, PosicaoAtual.Coluna - 3);
+
+                    if (Tab.GetPeca(p1) == null && Tab.GetPeca(p2) == null && Tab.GetPeca(p3) == null)
+                    {
+                        mat[PosicaoAtual.Linha, PosicaoAtual.Coluna - 2] = true;
+                    }
+                }
             }
 
             return mat;
